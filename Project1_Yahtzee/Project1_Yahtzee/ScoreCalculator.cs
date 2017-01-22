870bb8f1f266466d6b317ab37fe39a18283da741
Marc King <mjking@umich.edu>
2017-01-22 14:17:58 -0500

    Added comments to score calculator.

--- Project1_Yahtzee/Project1_Yahtzee/ScoreCalculator.cs
+++ Project1_Yahtzee/Project1_Yahtzee/ScoreCalculator.cs
@@ -14,17 +14,56 @@ namespace Project1_Yahtzee
     {
         #region Constants
 
+        /// <summary>
+        /// Number of points in the upper section required to earn the bonus.
+        /// </summary>
         private const int BONUS_REQUIREMENT = 63;
+
+        /// <summary>
+        /// Amount of points earned for the upper section bonus.
+        /// </summary>
         private const int BONUS_SCORE = 35;
+
+        /// <summary>
+        /// Number of dice with the same face required for the three of a kind score.
+        /// </summary>
         private const int THREE_OF_A_KIND_COUNT = 3;
+
+        /// <summary>
+        /// Number of dice with the same face required for the four of a kind score.
+        /// </summary>
         private const int FOUR_OF_A_KIND_COUNT = 4;
+
+        /// <summary>
+        /// Number of dice with the same face required for the Yahtzee score.
+        /// </summary>
         private const int YAHTZEE_COUNT = 5;
+
+        /// <summary>
+        /// Amount of points earned for the Yahtzee score.
+        /// </summary>
         private const int YAHTZEE_SCORE = 50;
+
+        /// <summary>
+        /// Amount of points earned for the full house score.
+        /// </summary>
         private const int FULL_HOUSE_SCORE = 25;
+
+        /// <summary>
+        /// Amount of points earned for the small straight score.
+        /// </summary>
         private const int SMALL_STRAIGHT_SCORE = 30;
+
+        /// <summary>
+        /// Amount of points earned for the large straight score.
+        /// </summary>
         private const int LARGE_STRAIGHT_SCORE = 40;
-        private const int MAX_SMALL_STRAIGHT_INDEX = 2;
-        private const int MAX_LARGE_STRAIGHT_INDEX = 1;
+
+        /// <summary>
+        /// Of the five dice, this is the maximum index the first die of a small straight can have
+        /// and still leave room to complete the small straight.
+        /// </summary>
+        private const int MAX_SMALL_STRAIGHT_INDEX = 1;
 
         #endregion Constants
 
@@ -40,24 +79,27 @@ namespace Project1_Yahtzee
         /// may have already been selected in the scorecard, but in this case increasing efficiency
         /// would require increasing coupling. This way, the scoring calculator doesn't need to 
         /// know anything about the rest of the program.
+        /// 
+        /// We could potentially have the GUI call only the scoring methods it needs, but then that
+        /// class would have to know a lot about this one.
         /// </remarks>
         public static Dictionary<ScoringCategory, int> CalculateDice(List<int> dice)
         {
             var scores = new Dictionary<ScoringCategory, int>();
 
-            scores.Add(ScoringCategory.Aces,          CalculateAces(dice));
-            scores.Add(ScoringCategory.Twos,          CalculateTwos(dice));
-            scores.Add(ScoringCategory.Threes,        CalculateThrees(dice));
-            scores.Add(ScoringCategory.Fours,         CalculateFours(dice));
-            scores.Add(ScoringCategory.Fives,         CalculateFives(dice));
-            scores.Add(ScoringCategory.Sixes,         CalculateSixes(dice));
-            scores.Add(ScoringCategory.ThreeOfAKind,  CalculateThreeOfAKind(dice));
-            scores.Add(ScoringCategory.FourOfAKind,   CalculateFourOfAKind(dice));
-            scores.Add(ScoringCategory.FullHouse,     CalculateFullHouse(dice));
+            scores.Add(ScoringCategory.Aces, CalculateAces(dice));
+            scores.Add(ScoringCategory.Twos, CalculateTwos(dice));
+            scores.Add(ScoringCategory.Threes, CalculateThrees(dice));
+            scores.Add(ScoringCategory.Fours, CalculateFours(dice));
+            scores.Add(ScoringCategory.Fives, CalculateFives(dice));
+            scores.Add(ScoringCategory.Sixes, CalculateSixes(dice));
+            scores.Add(ScoringCategory.ThreeOfAKind, CalculateThreeOfAKind(dice));
+            scores.Add(ScoringCategory.FourOfAKind, CalculateFourOfAKind(dice));
+            scores.Add(ScoringCategory.FullHouse, CalculateFullHouse(dice));
             scores.Add(ScoringCategory.SmallStraight, CalculateSmallStraight(dice));
             scores.Add(ScoringCategory.LargeStraight, CalculateLargeStraight(dice));
-            scores.Add(ScoringCategory.Yahtzee,       CalculateYahtzee(dice));
-            scores.Add(ScoringCategory.Chance,        CalculateChance(dice));
+            scores.Add(ScoringCategory.Yahtzee, CalculateYahtzee(dice));
+            scores.Add(ScoringCategory.Chance, CalculateChance(dice));
 
             return scores;
         }
@@ -75,7 +117,7 @@ namespace Project1_Yahtzee
                                   + scores[ScoringCategory.Fours]
                                   + scores[ScoringCategory.Fives]
                                   + scores[ScoringCategory.Sixes];
-            
+
             if (upperSectionScore >= BONUS_REQUIREMENT)
             {
                 return BONUS_SCORE;
@@ -90,42 +132,92 @@ namespace Project1_Yahtzee
 
         #region Private Methods
 
+        /// <summary>
+        /// Calculate the upper section score for a specific die face for a dice roll. The score is
+        /// the sum of all the die with the specified face.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <param name="face">Die face to calculate the score for.</param>
+        /// <returns>The calculated score for the die face.</returns>
         private static int CalculateUpperSectionCategory(List<int> dice, int face)
         {
             var faces = dice.FindAll(die => die == face);
             return faces.Count * face;
         }
 
+        /// <summary>
+        /// Calculate the upper section aces score. The score is the sum of all the dice with a one
+        /// showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated aces score.</returns>
         private static int CalculateAces(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Aces);
         }
 
+        /// <summary>
+        /// Calculate the upper section twos score. The score is the sum of all the dice with a two
+        /// showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated twos score.</returns>
         private static int CalculateTwos(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Twos);
         }
 
+        /// <summary>
+        /// Calculate the upper section threes score. The score is the sum of all the dice with a
+        /// three showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated threes score.</returns>
         private static int CalculateThrees(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Threes);
         }
 
+        /// <summary>
+        /// Calculate the upper section fours score. The score is the sum of all the dice with a
+        /// four showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated fours score.</returns>
         private static int CalculateFours(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Fours);
         }
 
+        /// <summary>
+        /// Calculate the upper section fives score. The score is the sum of all the dice with a
+        /// five showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated fives score.</returns>
         private static int CalculateFives(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Fives);
         }
 
+        /// <summary>
+        /// Calculate the upper section sixes score. The score is the sum of all the dice with a
+        /// six showing.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The calculated sixes score.</returns>
         private static int CalculateSixes(List<int> dice)
         {
             return CalculateUpperSectionCategory(dice, (int)ScoringCategory.Sixes);
         }
 
+        /// <summary>
+        /// Calculates the score for a roll containing a certain number of dice showing the same
+        /// face. The score is the sum of all of the dice if the minimum count is met.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <param name="count">Number of dice required to be showing the same face.</param>
+        /// <returns>The sum of all the die faces if requirements are met, or zero if not.</returns>
         private static int CalculateCountOfAKind(List<int> dice, int count)
         {
             foreach (var face in dice)
@@ -139,24 +231,42 @@ namespace Project1_Yahtzee
             return 0;
         }
 
+        /// <summary>
+        /// Calculates the score for rolling a three of a kind. The score is the sum of all of the
+        /// dice if there are three dice with the same face.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The sum of all the die faces if three contain the same face.</returns>
         private static int CalculateThreeOfAKind(List<int> dice)
         {
             return CalculateCountOfAKind(dice, THREE_OF_A_KIND_COUNT);
         }
 
+        /// <summary>
+        /// Calculates the score for rolling a four of a kind. The score is the sum of all of the
+        /// dice if there are four dice with the same face.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The sum of all the die faces if four contain the same face.</returns>
         private static int CalculateFourOfAKind(List<int> dice)
         {
             return CalculateCountOfAKind(dice, FOUR_OF_A_KIND_COUNT);
         }
 
-        /// Three of one face and two of another; worth 25 points.
+        /// <summary>
+        /// Calculates the score for rolling a full house; two of one face and three of another.
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The full house score if the requirements are met; zero otherwise.</returns>
         private static int CalculateFullHouse(List<int> dice)
         {
+            // We can assume that if there is a full house then the lowest index will contain one
+            // of the faces and the highest index with contain the other.
             var bottomFaces = dice.FindAll(die => die == dice[0]);
             var topFaces = dice.FindAll(die => die == dice[dice.Count - 1]);
 
-            if ((bottomFaces.Count == 2 && topFaces.Count == 3)
-                || (bottomFaces.Count == 3 && topFaces.Count == 2))
+            if ((bottomFaces.Count == 2 && topFaces.Count == 3) ||
+                (bottomFaces.Count == 3 && topFaces.Count == 2))
             {
                 return FULL_HOUSE_SCORE;
             }
@@ -166,9 +276,15 @@ namespace Project1_Yahtzee
             }
         }
 
-        /// Four sequential faces; worth 30 points. (e.g. 1-2-3-4, 2-3-4-5, 3-4-5-6)
+        /// <summary>
+        /// Calculates the score for rolling a small straight (four sequential dice).
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The small straight score if the requirements are met; zero otherwise.</returns>
         private static int CalculateSmallStraight(List<int> dice)
         {
+            // Can only be faces 1-2-3-4, 2-3-4-5, or 3-4-5-6
+            // Can only be indices 0-1-2-3 or 1-2-3-4
             for (int index = 0; index <= MAX_SMALL_STRAIGHT_INDEX; index++)
             {
                 if (dice[index + 1] == dice[index] + 1 &&
@@ -176,28 +292,37 @@ namespace Project1_Yahtzee
                     dice[index + 3] == dice[index] + 3)
                 {
                     return SMALL_STRAIGHT_SCORE;
-                }  
-            }
-            return 0;
-        }
-
-        /// Five sequential faces; worth 40 points. (e.g. 1-2-3-4-5, 2-3-4-5-6)
-        private static int CalculateLargeStraight(List<int> dice)
-        {
-            for (int index = 0; index <= MAX_LARGE_STRAIGHT_INDEX; index++)
-            {
-                if (dice[index + 1] == dice[index] + 1 &&
-                    dice[index + 2] == dice[index] + 2 &&
-                    dice[index + 3] == dice[index] + 3 &&
-                    dice[index + 4] == dice[index] + 4)
-                {
-                    return LARGE_STRAIGHT_SCORE;
                 }
             }
             return 0;
         }
 
-        /// All five dice show the same face; worth 50 points.
+        /// <summary>
+        /// Calculates the score for rolling a large straight (five sequential dice).
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The large straight score if the requirements are met; zero otherwise.</returns>
+        private static int CalculateLargeStraight(List<int> dice)
+        {
+            // Can only be faces 1-2-3-4-5 or 2-3-4-5-6
+            if (dice[1] == dice[0] + 1 &&
+                dice[2] == dice[0] + 2 &&
+                dice[3] == dice[0] + 3 &&
+                dice[4] == dice[0] + 4)
+            {
+                return LARGE_STRAIGHT_SCORE;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the score for rolling a Yahtzee (all five dice with the same face).
+        /// </summary>
+        /// <param name="dice">Sorted list of the die faces that have been rolled.</param>
+        /// <returns>The Yahtzee score if the requirement is met; zero otherwise.</returns>
         private static int CalculateYahtzee(List<int> dice)
         {
             if (CalculateCountOfAKind(dice, YAHTZEE_COUNT) > 0)
@@ -210,7 +335,11 @@ namespace Project1_Yahtzee
             }
         }
 
-        /// Sum of all dice with any combination.
+        /// <summary>
+        /// Calculates the score for taking a chance on the roll.
+        /// </summary>
+        /// <param name="dice">List of the die faces that have been rolled.</param>
+        /// <returns>Sum of all the die faces.</returns>
         private static int CalculateChance(List<int> dice)
         {
             return dice.Sum();
