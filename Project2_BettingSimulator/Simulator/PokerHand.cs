3869772951c3a25734a9a11da3ea5aaae2e469c7
Marc King <mjking@umich.edu>
2017-02-22 12:48:13 -0500

    Initial setup of a class to manage poker hands.

---
+++ Project2_BettingSimulator/Simulator/PokerHand.cs
@@ -0,0 +1,34 @@
+﻿// Marc King - mjking @umich.edu
+// CIS297 - Winter 2017 - Professor Eric Charnesky
+// University of Michigan - Dearborn
+
+using System;
+using System.Collections.Generic;
+
+namespace Simulator
+{
+    class PokerHand : IComparable<PokerHand>
+    {
+        private const int HAND_SIZE = 5;
+
+        private List<PokerCard> cards;
+
+        public PokerHand()
+        {
+            cards = new List<PokerCard>(HAND_SIZE);
+        }
+
+        public void Add(PokerCard card)
+        {
+            if (cards.Count < HAND_SIZE)
+            {
+
+            }
+        }
+
+        public int CompareTo(PokerHand other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
