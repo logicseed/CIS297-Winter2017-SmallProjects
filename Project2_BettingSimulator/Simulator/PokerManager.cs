ed5d78e3d17e7a9ea40b5f231521be9158e12652
Marc King <mjking@umich.edu>
2017-02-22 12:50:11 -0500

    Initial poker manager to handle betting on poker.

---
+++ Project2_BettingSimulator/Simulator/PokerManager.cs
@@ -0,0 +1,26 @@
+﻿// Marc King - mjking @umich.edu
+// CIS297 - Winter 2017 - Professor Eric Charnesky
+// University of Michigan - Dearborn
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    class PokerManager
+    {
+        private PokerHand playerHand;
+        private PokerHand computerHand;
+
+        public PokerManager()
+        {
+            playerHand = new PokerHand();
+            computerHand = new PokerHand();
+
+
+        }
+    }
+}
