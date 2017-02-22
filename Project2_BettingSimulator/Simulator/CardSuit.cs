ad0455a3b7956216540534dc0ef7aa534547bcab
Marc King <mjking@umich.edu>
2017-02-22 12:46:10 -0500

    Added card suit enumeration.

---
+++ Project2_BettingSimulator/Simulator/CardSuit.cs
@@ -0,0 +1,14 @@
+﻿// Marc King - mjking @umich.edu
+// CIS297 - Winter 2017 - Professor Eric Charnesky
+// University of Michigan - Dearborn
+
+namespace Simulator
+{
+    enum CardSuit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
